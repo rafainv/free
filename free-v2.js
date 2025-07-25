8f2cdc75e1aa039d4f960a4974f7de9ffd7a0e3e
rafaellrossi <91340426+rafaellrossi@users.noreply.github.com>
2025-07-25 01:26:32 -0300

    Update free-v2.js

--- free-v2.js
+++ free-v2.js
@@ -98,8 +98,6 @@ const free = async () => {
     await new Promise((r) => setTimeout(r, 5000));
     await page.screenshot({ path: "screen.png" });
     await browser.close()
-    assert.strictEqual(token !== null, true, "Cloudflare turnstile test failed!")
-
     await new Promise((r) => setTimeout(r, 5000));
 
     // roll
