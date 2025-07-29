0dbf805de1b3bda72b268090e76f5ac48db5b311
rafaellrossi <91340426+rafaellrossi@users.noreply.github.com>
2025-07-29 02:16:23 -0300

    Update free-v2.js

--- free-v2.js
+++ free-v2.js
@@ -24,7 +24,7 @@ const free = async () => {
     args: ["--start-maximized"],
     turnstile: true,
     headless: false,
-    disableXvfb: true,
+    disableXvfb: false,
     customConfig: {},
     connectOption: {
       defaultViewport: null,
